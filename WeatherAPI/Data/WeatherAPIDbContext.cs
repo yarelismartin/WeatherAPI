75e9a89153ea788148c520c612e7c9678e7a083c
Yarelis Martin <yarelis0martin@gmail.com>
2025-05-13 10:03:37 -0500

    created the DbContext for datbase access

---
+++ WeatherAPI/Data/WeatherAPIDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Data
+{
+    public class WeatherAPIDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Favorites> Favorites { get; set; }
+
+        public WeatherAPIDbContext(DbContextOptions<WeatherAPIDbContext> context) : base(context) 
+        { 
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<User>().HasData(UserData.Users);
+            //modelBuilder.Entity<Favorites>().HasData(FavoritesData.Favorites);
+            
+        }
+
+
+    }
+}
